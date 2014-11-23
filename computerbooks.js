cab1a93726c4fb80871e96e840dd6f2df92a8c58
Richard Eigenmann <richard.eigenmann@gmail.com>
2014-11-23 23:40:04 +0100

    Reverse order of Books and fixed minimum width layout issue

--- computerbooks.js
+++ computerbooks.js
@@ -8,8 +8,16 @@ angular.module('bookApp', ['angular.filter'])
             }])
         .controller('BookController', ['getBooks', function (getBooks) {
                 var self = this;
+                self.currentYear = new Date().getFullYear();
+                self.BaseYear = 2050;
                 getBooks.query().then(function (response) {
-                    self.books = response.data;
+                    var data = response.data;
+                    // Using a hack here because Angular always sorts keys alphabetically. So
+                    // I give it a key to sort on which transforms into the desired key
+                    // I.e. The Year 1986 translates to 2050-1986 = 84 which translates back 
+                    // as 2050 - 84 => 1986.
+                    data.map( function (item) { item.ReverseYear = self.BaseYear - item.ReadYear; })
+                    self.books = data;
                 });
             }])
         .directive('bookWidget', [function () {

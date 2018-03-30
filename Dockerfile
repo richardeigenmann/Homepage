1c37a5671c60b93fb4471dcc3fb9cfef766b7760
Richard Eigenmann <richard.eigenmann@gmail.com>
2018-03-30 21:52:01 +0200

    Dockerized the Homepage

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# docker build -t richardeigenmann/richinet .
+# docker run -it --rm --hostname richinet -v /richi/Src/RichiNet/weblog:/var/log/nginx/web -p8082:80 richardeigenmann/richinet
+
+FROM nginx:1.13
+RUN apt-get clean && apt-get update && apt-get install -y \
+  php \
+  php-mysqli \
+  less vim
+
+COPY vhost.conf /etc/nginx/conf.d/default.conf
+COPY *htm *html *js *css *gif *png *jpg /var/www/
+COPY Offline* /var/www/
+
+COPY generateComputerbooks.json.php /var/www
+WORKDIR /var/www
+RUN echo "192.168.1.104 gladius" >> /etc/hosts && php generateComputerbooks.json.php pw && rm generateComputerbooks.json.php
+
+RUN ls -ltr /var/www
+
+CMD nginx -g 'daemon off;'
+#ENTRYPOINT ["bash"]
--- generateComputerbooks.json.php
+++ generateComputerbooks.json.php
@@ -2,20 +2,17 @@
 // run from the command line as php generateComputerbooks.json.php dbpassword
 $dbhost = "gladius";
 $dbuser = "root";
+$dbname = "richi";
 $dbpass = $argv[1];
-$mysqli = new mysqli($dbhost, $dbuser, $dbpass, "richi");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-}
-$sql = "SELECT id, Author, Title, Source, Category, ImageUrl, BookUrl, ReadDate, year(ReadDate) as ReadYear from Books where Category = 'Computers'";
-$sth = mysqli_query($mysqli, $sql);
-$rows = array();
-while($r = mysqli_fetch_assoc($sth)) {
-    $rows[] = $r;
-}
+
+$db = new PDO("mysql:host=" . $dbhost . ";dbname=" . $dbname . ";charset=utf8mb4", $dbuser, $dbpass);
+$stmt = $db->query("SELECT id, Author, Title, Source, Category, ImageUrl, BookUrl, ReadDate, year(ReadDate) as ReadYear from Books where Category = 'Computers'");
+$results=$stmt->fetchAll(PDO::FETCH_ASSOC);
+$json=json_encode($results);
+$db=null;
 
 $myfile = fopen("computerbooks.json", "w") or die("Unable to open file!");
-fwrite($myfile, json_encode($rows));
+fwrite($myfile, $json);
 fclose($myfile);
 
 ?>
